8803cca6b1b3b4b1d0641cd951af8b3926505963
Mutaz Abu Ghazaleh <mutazag@hotmail.com>
2022-04-04 22:56:09 +1000

    read input dataset, downloaded by pipeline from ws

--- diabetes/src/train.py
+++ diabetes/src/train.py
@@ -1,6 +1,6 @@
 # %%
 import numpy as np
-from sklearn.datasets import load_diabetes
+# from sklearn.datasets import load_diabetes
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
@@ -9,15 +9,54 @@ import mlflow
 import mlflow.sklearn
 import pandas as pd
 
+
+#%% 
+# arguments=[
+#         '--kernel', 'linear', 
+#         '--penalty', 1.0,
+#         '--train_dataset', input_dataset.as_named_input('train')],
+
+import sys 
+print('############################### sys args')
+print(sys.argv)
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--kernel', type=str, dest='kernel')
+parser.add_argument('--penalty', dest='penalty')
+parser.add_argument('--train_dataset', dest='train_dataset')
+parser.add_argument('--dataset_download', dest='dataset_download')
+
+args = parser.parse_args()
+train_dataset = args.train_dataset
+print('###############################')
+print('train dataset : ', train_dataset)
+mlflow.set_tag('train_dataset', train_dataset)
+mlflow.set_tag('kernel', args.kernel)
+mlflow.set_tag('penalty', args.penalty)
+mlflow.set_tag('dataset_download', args.dataset_download)
+
+#%% 
+import os
+print('################################ download dir content')
+print(os.listdir(args.dataset_download))
+mlflow.log_dict({'files': os.listdir(args.dataset_download)}, 'downloadfiles.json')
+
+df_input = pd.read_parquet(args.dataset_download)
+print(df_input.head())
+
+X = np.array(df_input.drop(columns=['target']))
+y = np.array(df_input.target)
+print('read features: ', len(X))
+print('read target: ', len(y))
 #%%
 # diabetes = load_diabetes(as_frame=True)
 # df = diabetes.data
 # df['target'] = diabetes.target
 # df.to_csv("diabetes.csv", index=False)
 #%%
-df = pd.read_csv("diabetes.csv")
-X = np.array(df.drop(columns=['target']))
-y = np.array(df.target)
+# df = pd.read_csv("diabetes.csv")
+# X = np.array(df.drop(columns=['target']))
+# y = np.array(df.target)
 #%%
 alpha = .5
 
@@ -48,6 +87,7 @@ mlflow.sklearn.log_model(regression_model, "diabetes_regression_model")
 
 #%%
 # Plot actuals vs predictions and save the plot within the run
+import matplotlib.pyplot as plt
 fig = plt.figure(1)
 idx = np.argsort(data["test"]["y"])
 plt.plot(data["test"]["y"][idx], preds[idx])
