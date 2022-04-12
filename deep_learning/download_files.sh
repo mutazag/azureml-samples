ecc1ac318ab7c7326ab8abc2c25c640587e126a8
Mutaz Abu Ghazaleh <mutazag@hotmail.com>
2022-04-12 12:22:20 +0000

    updated download and unzip script

--- deep_learning/download_files.sh
+++ deep_learning/download_files.sh
@@ -1,2 +1,5 @@
-wget https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip  ~/cloudfiles/data/
-unzip  ~/cloudfiles/data/cats_and_dogs_filtered.zip  ~/cloudfiles/data/.
+wget https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip  -P ~/cloudfiles/data/.
+
+unzip  ~/cloudfiles/data/cats_and_dogs_filtered.zip -d ~/cloudfiles/data
+
+chmod 777 ~/cloudfiles/data/cats_and_dogs_filtered
